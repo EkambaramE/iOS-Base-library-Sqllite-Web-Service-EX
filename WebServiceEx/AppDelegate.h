f59f2f49cea1c5d8c2b487a4510d434ef673d9dd
Krishna_Prabha <krishnaprabhas@karyatech.com>
2015-08-21 15:11:23 +0530

    Initial Commit

---
+++ WebServiceEx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebServiceEx
+//
+//  Created by EkambaramE on 21/08/15.
+//  Copyright (c) 2015 EkambaramE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebServiceExTests/WebServiceExTests.m
@@ -0,0 +1,40 @@
+//
+//  WebServiceExTests.m
+//  WebServiceExTests
+//
+//  Created by EkambaramE on 21/08/15.
+//  Copyright (c) 2015 EkambaramE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WebServiceExTests : XCTestCase
+
+@end
+
+@implementation WebServiceExTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
